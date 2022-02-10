a4a050c1654728c211d34754a5caa5f9b9413204
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-10 20:44:32 +0000

    updated to include ext 2 criteria

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const logo = document.querySelector('.konami-logo')
 const sequenceOneCheck = ['ArrowUp','ArrowUp','ArrowDown','ArrowDown','ArrowLeft','ArrowRight','ArrowLeft','ArrowRight','b','a','Enter']
 const sequenceTwoCheck = ['ArrowRight','ArrowLeft','ArrowDown','ArrowUp','a','b','Enter']
 
+const sequenceOneText = ['↑','↑','↓','↓','←','→','←','→','B','A','Enter']
+const sequenceTwoText = ['→','←','↓','↑','A','B','Enter']
+
 document.body.append(konamiCode)
 konamiCode.append(buttonFirst, buttonSecond)
 
@@ -17,21 +20,22 @@ buttonFirst.innerText = "Code A"
 buttonSecond.innerText = "Code B"
 
 buttonFirst.addEventListener('click', function() {
-konamiCode.innerText = "↑ ↑ ↓ ↓ ← → ← → B A Enter" 
-keyLogic(sequenceOneCheck)
+konamiCode.innerHTML = "<span>↑</span> ↑ ↓ ↓ ← → ← → B A Enter" 
+keyLogic(sequenceOneCheck, sequenceOneText)
 })
 
 buttonSecond.addEventListener('click', function() {
-konamiCode.innerText = "→ ← ↓ ↑ A B Enter"
-keyLogic(sequenceTwoCheck)
+konamiCode.innerHTML = "<span>→</span> ← ↓ ↑ A B Enter" 
+keyLogic(sequenceTwoCheck, sequenceTwoText)
 })
 
 
-function keyLogic(sequence) {
+function keyLogic(sequence, text) {
 let sequenceArray = []
 document.addEventListener("keydown", function(event) {
+    
     sequenceArray.push(event.key)
-   
+
     if(checkIncorrectSequence(sequenceArray, sequence)) {
         logo.classList.remove("combo")
         sequenceArray = []
@@ -42,6 +46,8 @@ document.addEventListener("keydown", function(event) {
         logo.classList.add("combo")
         sequenceArray = []
     }
+
+    generateHTML(sequenceArray, text)
 
 });
 
@@ -60,4 +66,18 @@ function checkIncorrectSequence(currentSequence, sequence) {
 
 function correctCode(currentSequence, sequence) {
     return currentSequence.length === sequence.length
+}
+
+function generateHTML(currentSequence, sequenceText) {
+    let html = ``
+    for (let i = 0; i < sequenceText.length; i++) {
+        if(currentSequence.length === i) {
+            html += `<span>${sequenceText[i]}</span> `
+        }
+        else {
+            html += `${sequenceText[i]} `
+        }
+    }
+
+    konamiCode.innerHTML = html
 }
